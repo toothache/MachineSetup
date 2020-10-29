b02f70c4b8c95d2635bc0125a98294664eb333ab
Yateng Hong <yatengh@microsoft.com>
2020-10-29 17:37:04 +0800

    Use oh-my-posh to configure theme.

--- profiles/powershell/profile.ps1
+++ profiles/powershell/profile.ps1
@@ -2,16 +2,20 @@ if (test-path env:\SkipLoadProfile) {
     return
 }
 
+
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
+
 
 function Show-Welcome {
     $time = Get-Date
     $psVersion= $host.Version.Major
     $curUser= (Get-ChildItem Env:\USERNAME).Value
     $curComp= (Get-ChildItem Env:\COMPUTERNAME).Value
+
+    $defaultForegroundColor = "Green"
 
     Write-Host "Greetings, $curUser!" -foregroundColor $defaultForegroundColor
     Write-Host "It is: $($time.ToLongDateString())" -foregroundColor $defaultForegroundColor
@@ -26,76 +30,21 @@ function Show-Welcome {
     Write-Host "Happy scripting!" `n -foregroundColor $defaultForegroundColor
 }
 
-function Prompt {
-    $origLastExitCode = $LastExitCode
-
-    $prompt = ""
-    $prompt += Write-Prompt "$env:USERNAME@" -NoNewline -ForegroundColor $defaultForegroundColor
-
-    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
-    if ($curPath.ToLower().StartsWith($Home.ToLower())) {
-        $curPath = "~" + $curPath.SubString($Home.Length)
-    }
-
-    $curDirectory = [System.IO.Path]::GetFileName($curPath)
-    if ($curDirectory -eq "") {
-        $curDirectory = $curPath
-    }
-    
-    $prompt += Write-Prompt $curDirectory -NoNewline -ForegroundColor $directoryColor
-
-    if (Test-Administrator) { # if elevated
-        $prompt += Write-Prompt " [ADMIN]" -ForegroundColor $emphasisColor
-    }
-
-    $prompt += Write-Prompt "$(if ($PsDebugContext) {' [DBG]'} else {''})" -NoNewline -ForegroundColor $emphasisColor
-
-    $prompt += Write-VcsStatus
-
-    if (Get-GitStatus -Force) {
-        # start new line if there're available git status
-        $prompt += Write-Prompt "`n"
-    }
-    else {
-        $prompt += Write-Prompt " "
-    }
-
-    $prompt += Write-Prompt "$('>' * ($nestedPromptLevel + 1))" -NoNewline -ForegroundColor $defaultForegroundColor
-
-    $host.UI.RawUI.WindowTitle = "User: $curUser | Current DIR: $((Get-Location).Path)"
-
-    $LastExitCode = $origLastExitCode
-
-    Return " "
-}
 
 function ChangeConsoleTheme()
 {
-    Import-Module PSConsoleTheme
-    Set-ConsoleTheme 'Monokai'
-
-    $GitPromptSettings.BranchIdenticalStatusToForegroundColor = "DarkBlue"
-    $GitPromptSettings.BranchAheadStatusForegroundColor = "DarkGreen"
-    $GitPromptSettings.BranchBehindStatusForegroundColor = "DarkRed"
-    $GitPromptSettings.BranchForegroundColor = "DarkYellow"
-    $GitPromptSettings.WorkingForegroundColor = "DarkYellow"
+    Import-Module oh-my-posh
+    Set-Theme Paradox
 }
 
 Import-Module ZLocation
 Import-Module posh-git
 
-if (-not (test-path env:\VSCODE*) -and -not (test-path env:\SkipLoadTheme)) {
-    $defaultForegroundColor = "DarkGreen"
+if (-not (test-path env:\SkipLoadTheme)) {
     ChangeConsoleTheme
 }
 else {
     Write-Host "Skip changing themes."
-    $defaultForegroundColor = "Green"
-
-    $GitPromptSettings.BranchIdenticalStatusToForegroundColor = "Blue"
-    $GitPromptSettings.BranchAheadStatusForegroundColor = "Green"
-    $GitPromptSettings.BranchBehindStatusForegroundColor = "Red"
-    $GitPromptSettings.WorkingForegroundColor = "Yellow"
 }
 
 Show-Welcome;
@@ -103,9 +52,19 @@ Show-Welcome;
 $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
 if (Test-Administrator) {
     if ($curPath.ToLower() -eq "c:\windows\system32") {
-        Set-Location ~\Source
+        Set-Location d:\source
     }
+} elseif ($curPath.ToLower() -eq $Home.ToLower()) {
+    Set-Location d:\source
 }
-elseif ($curPath.ToLower() -eq $Home.ToLower()) {
-    Set-Location ~\Source
-}
+
+
+$CONDA_ROOT = "C:\Users\yatengh\Miniconda3"
+$Env:CONDA_EXE = "$CONDA_ROOT\Scripts\conda.exe"
+
+Import-Module "$CONDA_ROOT\shell\condabin\Conda.psm1"
+conda activate base
+
+
+# PatchPrompt
+Add-CondaEnvironmentToPrompt
